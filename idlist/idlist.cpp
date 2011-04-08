7e3911327cd02808cf0fc448a2510052534139f9
Chris Sutcliffe <ir0nh34d@gmail.com>
2011-04-07 23:07:27 -0400

    Make idlist.cpp MinGW friendly

--- idlist/idlist.cpp
+++ idlist/idlist.cpp
@@ -1,40 +1,61 @@
+#define UNICODE
+
 #include <windows.h>
 #include <shlobj.h>
 #include <stdio.h>
 #include <Shlwapi.h>
 
-int WINAPI WinMain (HINSTANCE hInstance,
-                    HINSTANCE hPrevInstance,
-                    LPSTR lpCmdLine,
-                    int nCmdShow)
+#ifdef __GNUC__
+#define UNUSED __attribute__((unused))
+#else
+#define UNUSED
+#endif
+
+typedef HANDLE (WINAPI *lpfnSHAllocShared)(const void*, DWORD, DWORD);
+typedef BOOL (WINAPI *lpfnSHFreeShared)(HANDLE, DWORD);
+
+int WINAPI WinMain (HINSTANCE hInstance UNUSED,
+                    HINSTANCE hPrevInstance UNUSED,
+                    LPSTR lpCmdLine UNUSED,
+                    int nCmdShow UNUSED)
 {
-	HANDLE sharedMem = NULL;
-	DWORD currentProcess = GetCurrentProcessId();
-	LPITEMIDLIST pidl = NULL;
-	WCHAR command[MAX_PATH];
-	SHELLEXECUTEINFO sei;
+  HANDLE sharedMem = NULL;
+  DWORD currentProcess = GetCurrentProcessId();
+  LPITEMIDLIST pidl = NULL;
+  WCHAR command[MAX_PATH];
+  SHELLEXECUTEINFO sei;
+  HMODULE shlwapiDLL = LoadLibrary(L"shlwapi.dll");
+  int ret = 1;
+  lpfnSHAllocShared SHAllocShared = (lpfnSHAllocShared)GetProcAddress(shlwapiDLL, "SHAllocShared");
+  lpfnSHFreeShared SHFreeShared = (lpfnSHFreeShared)GetProcAddress(shlwapiDLL, "SHFreeShared");
 
-	ZeroMemory(&sei, sizeof(sei));
-	sei.cbSize = sizeof(sei);
-	sei.fMask = SEE_MASK_NOASYNC | SEE_MASK_UNICODE;
-	sei.lpFile = L"C:\\Windows\\Explorer.exe";
-	sei.nShow = SW_SHOW;
-	sei.hwnd = GetDesktopWindow();
+  ZeroMemory(&sei, sizeof(sei));
+  sei.cbSize = sizeof(sei);
+  sei.fMask = SEE_MASK_UNICODE | SEE_MASK_NOCLOSEPROCESS;
+  sei.lpFile = L"c:\\windows\\explorer.exe";
+  sei.nShow = SW_SHOW;
+  sei.hwnd = GetDesktopWindow();
 
-	if (SUCCEEDED(SHGetFolderLocation(NULL, CSIDL_DRIVES, NULL, 0, &pidl)))
-	{
-		sharedMem = SHAllocShared(pidl, ILGetSize(pidl), currentProcess);
-		ILFree(pidl);
+  if (SHAllocShared && SHFreeShared)
+    {
+      if (SUCCEEDED(SHGetFolderLocation(NULL, CSIDL_DRIVES, NULL, 0, &pidl)))
+        {
+          sharedMem = SHAllocShared(pidl, ILGetSize(pidl), currentProcess);
+          ILFree(pidl);
 
-		if (sharedMem)
-		{
-			swprintf_s(command, L"/idlist,:%d:%d", sharedMem, currentProcess);
-			sei.lpParameters = command;
-			ShellExecuteEx(&sei);
-			Sleep(100);
-			return 0;
-		}
-	}
+          if (sharedMem)
+            {
+              swprintf(command, L"/idlist,:%d:%d", sharedMem, currentProcess);
+              sei.lpParameters = command;
+              ShellExecuteEx(&sei);
+              WaitForSingleObject(sei.hProcess, INFINITE);
+              SHFreeShared(sharedMem, currentProcess);
+              ret = 0;
+            }
+        }
+    }
 
-	return 1;
+  FreeLibrary(shlwapiDLL);
+
+  return ret;
 }
